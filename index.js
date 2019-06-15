2cc21dba486e6d2296e2b2996e1edc7bad9af9b6
Isaza156 <jonathan.isaza@hotmail.com>
2019-06-15 18:01:08 -0500

    act

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ if (edad >= 18){
             console.log(nombre + " Hola")
 }
 }
+
+
+var nombre = "Jhon"
+var edad = 15
+var dias = 0, horas = 0
+if (edad >= 18){
+    dias = edad * 365
+    console.log(nombre + " a vivido " + dias)
+}else if(edad % 2 == 0){
+        horas = edad * 365 * 24
+        console.log("A vivido " + horas)
+        }else{
+
+            console.log(nombre + " Hola")
+}
